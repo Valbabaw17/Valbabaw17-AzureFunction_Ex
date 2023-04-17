ff86e8a90920abedc8d2ac0ed7281faefc027631
Valerio17 <106080454+Valbabaw17@users.noreply.github.com>
2023-04-17 14:19:12 +0200

    Add lastname on the title

--- src/manga-app/src/App.tsx
+++ src/manga-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Valerio
+          Hello Walbert Valerio
         </p>
         <a
           className="App-link"
